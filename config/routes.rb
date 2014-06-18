c31a3a20ac2cfe087495cee58692292cb0389c9e
AndrewCap <andrei.capastru@gmail.com>
2014-06-18 21:45:48 +0300

    Added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#me'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,9 +16,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  LearnRails::Application.routes.draw do 
-    root to: redirect('index.html')
-  end
+
   # Example resource route with options:
   #   resources :products do
   #     member do
